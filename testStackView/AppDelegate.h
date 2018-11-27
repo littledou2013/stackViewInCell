f888508a45519f27d2bf0fcc6d6fbf9ad4ba32eb
chenxiaoshuang <chenxiaoshuang1@corp.netease.com>
2018-11-27 13:14:49 +0800

    Initial Commit

---
+++ testStackView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testStackView
+//
+//  Created by chenxiaoshuang on 2018/11/27.
+//  Copyright © 2018年 chenxiaoshuang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
